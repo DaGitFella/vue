7846bb4b3118395fd30f33540b94fa5b6d354846
DaGitFella <davibezerra123457@gmail.com>
2025-10-09 23:27:31 -0300

    parte 2 finalizada

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1>Parte 1</h1>
   <div>
     <p :class="classContadorUm">Contador: {{ contador }}</p>
     <button @click="incrementar">Incrementar</button>
@@ -12,7 +13,8 @@
   </div>
 
   <p>Soma dos contadores: {{ somaContadores }}</p>
-
+  <p>----------------------------------------------------------------------------------------</p>
+  <h1>Parte 2</h1>
   <form @submit.prevent="adicionar">
     <input v-model.trim="novaMensagem" placeholder="Digite uma mensagem" />
     <button :disabled="!novaMensagem">Adicionar</button>
@@ -24,20 +26,60 @@
 
   <ul v-if="mensagens.itens.length">
     <li v-for="mensagem in mensagensFiltradas" :key="mensagem.id">
-      <p :class="{ longo: mensagem.texto.length > 30}">{{ mensagem.id }} -- {{ mensagem.texto }}</p>
-      <button @click="remover(mensagem.id)">x</button>
+      <div v-if="idItemEditando !== mensagem.id" class="estiloBotoes">
+        <p :class="{ longo: mensagem.texto.length > 30}">{{ mensagem.id }} -- {{ mensagem.texto }}</p>
+        <button @click="remover(mensagem.id)">x</button>
+        <button @click="editarMensagem(mensagem.id)">editar</button>
+      </div>
+
+      <div v-if="idItemEditando === mensagem.id" class="estiloBotoes">
+        <input v-model="mensagem.texto">
+        <button @click="idItemEditando = null">Salvar</button>
+      </div>
+
     </li>
+
     <p>exibindo {{mensagensFiltradas.length}} de
       {{mensagens.itens.length}}</p>
   </ul>
   <p v-else>Lista vazia</p>
   <button @click="limpar" v-if="mensagens.itens.length">Limpar lista</button>
-
-
+  <p>----------------------------------------------------------------------------------------</p>
 </template>
 
 <script setup>
 import {ref, computed, reactive} from 'vue'
+
+// Parte 1
+
+const contador = ref(0)
+
+const contadorDois = ref(1)
+
+const somaContadores = computed(() => contador.value + contadorDois.value)
+
+const classContadorUm = computed(() => contador.value > contadorDois.value ? 'destaque': '')
+const classContadorDois = computed(() => contadorDois.value > contador.value ? 'destaque': '')
+
+const incrementar = () => {
+  contador.value++
+}
+
+const decrementar = () => {
+  contador.value > 0 ? contador.value-- : 0
+}
+
+const incrementar2 = () => {
+  contadorDois.value++
+}
+
+const decrementar2 = () => {
+  contadorDois.value > 0 ?contadorDois.value -- : 0
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+// Parte 2
 
 let mensagens = reactive({
   nextId: 1,
@@ -65,43 +107,24 @@ const novaMensagem = ref('')
 
 const adicionar = () => {
   if (novaMensagem.value) {
-    mensagens.itens.push({id: mensagens.nextId++, texto: novaMensagem.value})
+    mensagens.itens.push({id: mensagens.nextId++, texto: novaMensagem.value, editando: false})
+  } else {
+    erro.value = 'o campo enviado estava vazio'
   }
-
-  erro.value = 'o campo enviado estava vazio'
 }
 
 function remover(id) {
   mensagens.itens = mensagens.itens.filter(m => m?.id !== id)
 }
 
+const idItemEditando = ref(null)
+
+function editarMensagem(id) {
+  idItemEditando.value = id
+}
+
 const limpar = () => {
   mensagens.itens = []
-}
-
-const contador = ref(0)
-
-const contadorDois = ref(1)
-
-const somaContadores = computed(() => contador.value + contadorDois.value)
-
-const classContadorUm = computed(() => contador.value > contadorDois.value ? 'destaque': '')
-const classContadorDois = computed(() => contadorDois.value > contador.value ? 'destaque': '')
-
-const incrementar = () => {
-  contador.value++
-}
-
-const decrementar = () => {
-  contador.value > 0 ? contador.value-- : 0
-}
-
-const incrementar2 = () => {
-  contadorDois.value++
-}
-
-const decrementar2 = () => {
-  contadorDois.value > 0 ?contadorDois.value -- : 0
 }
 
 </script>
@@ -127,5 +150,13 @@ li {
 .longo {
   color: green;
   font-weight: bold;
+}
+
+.estiloBotoes {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin: 0;
+  padding: 0;
 }
 </style>
