5c91cfe3c7bcdf05aebf20c3f1d7822d0fd9316e
DaGitFella <davibezerra123457@gmail.com>
2025-11-04 14:49:36 -0300

    working on unit 3

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
+  <h1>Aplicação Vue com componentes</h1>
+
+  <div class="show-buttons-list">
     <button @click="exibirAtividadeUm = !exibirAtividadeUm">
       {{ exibirAtividadeUm ? 'Ocultar' : 'Mostrar' }} atividade 1
     </button>
     <button @click="exibirAtividadeDois = !exibirAtividadeDois">
       {{ exibirAtividadeDois ? 'Ocultar' : 'Mostrar' }} atividade 2
+    </button>
+    <button @click="exibirAtividadeTres = !exibirAtividadeTres">
+      {{ exibirAtividadeTres ? 'Ocultar': 'Mostrar' }} atividade 3
     </button>
   </div>
 
@@ -17,15 +22,30 @@
     <h2>Listagem simples</h2>
     <atividade-dois />
   </div>
+
+  <div v-if="exibirAtividadeTres">
+    <h2>Exibição condicional</h2>
+
+    <Mensagem v-if="mostrar"/>
+    <p v-else>Nenhuma mensagem registrada.</p>
+
+    <button @click="mostrar = !mostrar">
+      Alterar exibição
+    </button>
+
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import atividadeUm from './components/atividadeUm.vue';
 import atividadeDois from './components/atividadeDois.vue';
+import Mensagem from './components/Mensagem.vue';
 
 const exibirAtividadeUm = ref(false)
 const exibirAtividadeDois = ref(false)
+const exibirAtividadeTres = ref(false)
+const mostrar = ref(false)
 
 </script>
 
@@ -33,6 +53,11 @@ const exibirAtividadeDois = ref(false)
 
 h2 {
   text-align: center;
+}
+
+.show-buttons-list {
+  display: flex;
+  gap: 20px;
 }
 
 </style>
